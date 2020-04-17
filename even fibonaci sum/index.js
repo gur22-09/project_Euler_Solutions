2b8df017090aab930f330def6f404bb89c62deab
GURPRIT SINGH <gurprit.business94@gmail.com>
2020-04-17 19:33:44 +0530

    updated solution

--- even fibonaci sum/index.js
+++ even fibonaci sum/index.js
@@ -1,16 +1,16 @@
 function evenFibsum(n){
-  let fib3 = 2;
-  let fib6 = 0;
-  let result = 2;
-  let sum = 0;
-
-  while(sum <n){
-    sum+=result;
-    
-    result = 4*fib3+fib6;
-    fib6=fib3;
-    fib3=result;
-  } 
-
-  return sum;
+ if (n <= 1) {
+    return 0;
+  } else {
+    let nMinus2 = 1, nMinus1 = 2, value = 3, sum = 2;
+    while(value < n) {  
+      if(value % 2 === 0) {
+        sum += value;
+      }
+      nMinus2 = nMinus1;
+      nMinus1 = value;
+      value = nMinus1 + nMinus2;
+    }
+    return sum;
+  }
 }
