306ccd21dd449e99c526ac7afd3d82cc2c0fcaf8
GURPRIT SINGH <gurprit.business94@gmail.com>
2020-04-20 17:45:47 +0530

    Nth prime number

---
+++ nth prime number/index.js
@@ -0,0 +1,27 @@
+const nthPrime = n =>{
+  let primeNum = [];
+  
+  if(n<=0 || typeof(n) !== 'number') return undefined;
+
+  for(let i=2;primeNum.length<n;i++){
+    if(isPrime(i)){
+      primeNum.push(i);
+       
+    }
+  }
+   
+  return primeNum;
+}
+
+
+function isPrime(num){
+  for(let i=2;i<num;i++){
+    if(num%i === 0){
+      return false;
+    }
+  }
+
+  return true;
+}
+
+100thPrimeNum = nthePrime(100);
