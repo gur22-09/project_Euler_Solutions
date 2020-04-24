b4393a1183d7a978511d542a5079a5251ab4177f
GURPRIT SINGH <gurprit.business94@gmail.com>
2020-04-24 20:45:59 +0530

    added #9

--- special Pythagorean triplet/index.js
+++ special Pythagorean triplet/index.js
@@ -1,1 +1,15 @@
 //there is only one pythagorean triplet that satisfies the condition a+b+c = 1000, find the product abc.
+function triplet(n){
+ let sum = n;
+
+ for(let a=0;a<n;a++){
+     for(let b=a+1;b<n;b++){
+         let c = sum -a-b;
+         if(c<a+b){
+             if(Math.pow(c,2)===Math.pow(a,2)+Math.pow(b,2)){
+                 return a*b*c;
+             }
+         }
+     }
+ }
+}
