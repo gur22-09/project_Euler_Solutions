4e4bae7cca4fe6bfa0491f709aa10c0facd98f4e
GURPRIT SINGH <gurprit.business94@gmail.com>
2020-05-14 22:11:33 +0530

    updated

--- search insert position/index.js
+++ search insert position/index.js
@@ -1,19 +1,15 @@
+
 var searchInsert = function(nums, target) {
-   let index=0; 
-   if(nums.includes(target)){
-       index = nums.find((num,index)=>{
-        if(num ===target) return index;
-    });
-   }else{
-       const indexMinus1 = nums.find((num,index)=>{
-           if(num>target) return index;
-       });
-       
-       index = indexMinus1 - 1;
-       
-   }
+    if (target < nums[0]) return 0;
+    if (target > nums[nums.length-1]) return nums.length;
     
-    
-   return index; 
-   
+    for (let i=0 ; i<nums.length ; i++) {
+        if (nums[i] === target || (nums[i] > target && nums[i-1] < target)) {
+            return i;
+        } else if (nums[i] > target) {
+            return i-1
+        }
+    }
 };
+   
+
